9df4e98993d88adca93c48cb4f0e4ef41b40b7cc
saad09092012 <msaadatif@gmail.com>
2025-08-30 13:44:03 +0400

    A3

---
+++ A3.py
@@ -0,0 +1,21 @@
+#Input a number
+num = int(input("Enter the number : "))
+t = num
+numLen = 0
+#iterate the loop
+while t>0:
+    numLen = numLen+1
+    t = int(t/10)
+
+if numLen>=4: #condition 1
+    numLen = int(numLen/2)
+    chk = 0
+    while num>0: #iterate loop
+        rem = num%10
+        if chk==numLen: #nested condition 1
+            midOne = rem
+        elif chk==(numLen-1):
+            midTwo = rem
+        num = int(num/10)
+        chk = chk+1
+    prod = midOne*midTwo #
